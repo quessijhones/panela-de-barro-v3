dd0181c66a9047d816b064ab2d7d3016c23a213d
quessijhones <quessijhones@gmail.com>
2025-08-23 13:37:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Mantém caminhos absolutos funcionando no Vercel
-  server: { port: 5173, open: true },
-});
+  server: { port: 5173 },
+  preview: { port: 5174 },
+})
