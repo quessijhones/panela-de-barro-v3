83e670f8103c31a1bb163906be8e9c96e518d689
quessijhones <quessijhones@gmail.com>
2025-08-26 13:10:40 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./styles.css";
+import { initLangFromURL, applyDocumentLang, getLang } from "./i18n";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+// 1) inicializa a partir de ?lang= se houver
+initLangFromURL();
+// 2) garante <html lang/dir> correto no primeiro paint
+applyDocumentLang(getLang());
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
