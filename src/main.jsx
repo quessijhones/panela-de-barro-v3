199c30fae534c830e50b569f4e6c2e209e16fc69
quessijhones <quessijhones@gmail.com>
2025-08-26 03:13:15 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./styles.css"; // <- garante estilos
 
 createRoot(document.getElementById("root")).render(<App />);
