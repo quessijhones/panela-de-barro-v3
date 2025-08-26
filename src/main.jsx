38850a7ae6e7201e6c379480242eb6c46dd333d0
quessijhones <quessijhones@gmail.com>
2025-08-26 14:00:32 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
+// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+
+// estas funções agora existem no i18n.js (acabamos de adicioná-las)
 import { initLangFromURL, applyDocumentLang, getLang } from "./i18n";
 
 // 1) inicializa a partir de ?lang= se houver
 initLangFromURL();
-// 2) garante <html lang/dir> correto no primeiro paint
+
+// 2) aplica <html lang> e dir (rtl para árabe)
 applyDocumentLang(getLang());
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 3) monta a aplicação
+const rootEl = document.getElementById("root");
+createRoot(rootEl).render(<App />);
+
+// 4) (opcional) ouça mudanças de idioma para atualizar o <html lang/dir>
+window.addEventListener?.("langchange", () => {
+  applyDocumentLang(getLang());
+});
